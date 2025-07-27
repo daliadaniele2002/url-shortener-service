fea5e694300914c7d02b6b5d8c7d715e6fbf0284
daniele <daliadaniele2002@gamil.com>
2025-07-27 22:23:25 +0200

    Add main class

---
+++ encode-service/src/main/java/daniele/dalia/urlshortenerservice/encodeservice/EncodeServiceApplication.java
@@ -0,0 +1,13 @@
+package daniele.dalia.urlshortenerservice.encodeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncodeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EncodeServiceApplication.class, args);
+    }
+
+}
